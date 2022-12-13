198b78fdd0ab683a6542c32450dc5ca66f94c946
Piyush <Piyush.Singh@khtechnologies.in>
2022-12-13 14:13:58 +0530

    initial commit

---
+++ src/ducks/products.js
@@ -0,0 +1,12 @@
+//reducer
+export default function products(state = []) {
+    return state;
+}
+//selectors
+export function getProducts(state,props){
+    return state.products;
+}
+
+export function getProduct(state,props){
+    return state.products.find((item)=> item.id === props.id)
+}
